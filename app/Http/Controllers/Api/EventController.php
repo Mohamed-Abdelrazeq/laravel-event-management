8c47d07c8ff1606f09cbc851bbc2595ab5720ffc
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-11-07 12:26:35 +0200

    include query

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,13 +14,34 @@ class EventController extends Controller
      */
     public function index()
     {
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
         return EventResource::collection(
-            Event::with(
-                'user',
-            )->paginate(10)
+            $query->latest()->paginate(10)
         );
     }
 
+
+    protected function shouldIncludeRelation(string $relation)
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(",", $include));
+
+        return in_array($relation, $relations);
+    }
     /**
      * Store a newly created resource in storage.
      */
